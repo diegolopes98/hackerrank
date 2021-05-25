ecdee44464f52ee546e9416775376fe766e8fee3
Diego Lopes <diegoplopes5@gmail.com>
2021-05-25 02:05:36 -0300

    feat: add algorithms implementation apple and orange

---
+++ src/Algorithms/Implementation/AppleAndOrange/Solution.java
@@ -0,0 +1,73 @@
+package Algorithms.Implementation.AppleAndOrange;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class Result {
+
+    /*
+     * Complete the 'countApplesAndOranges' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER s
+     *  2. INTEGER t
+     *  3. INTEGER a
+     *  4. INTEGER b
+     *  5. INTEGER_ARRAY apples
+     *  6. INTEGER_ARRAY oranges
+     */
+
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+        int appleCount = (int) apples
+                .stream()
+                .map(apple -> apple + a)
+                .filter(applesPos -> applesPos >= s && applesPos <= t)
+                .count();
+
+        int orangeCount = (int) oranges
+                .stream()
+                .map(orange -> orange + b)
+                .filter(orangesPos -> orangesPos >= s && orangesPos <= t)
+                .count();
+
+        System.out.println(appleCount);
+        System.out.println(orangeCount);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String[] firstMultipleInput = in.next().replaceAll("\\s+$", "").split(" ");
+
+        int s = Integer.parseInt(firstMultipleInput[0]);
+
+        int t = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] secondMultipleInput = in.next().replaceAll("\\s+$", "").split(" ");
+
+        int a = Integer.parseInt(secondMultipleInput[0]);
+
+        int b = Integer.parseInt(secondMultipleInput[1]);
+
+        String[] thirdMultipleInput = in.next().replaceAll("\\s+$", "").split(" ");
+
+        int m = Integer.parseInt(thirdMultipleInput[0]);
+
+        int n = Integer.parseInt(thirdMultipleInput[1]);
+
+        List<Integer> apples = Stream.of(in.next().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        List<Integer> oranges = Stream.of(in.next().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        Result.countApplesAndOranges(s, t, a, b, apples, oranges);
+    }
+}
