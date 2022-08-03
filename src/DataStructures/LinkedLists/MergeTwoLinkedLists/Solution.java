37dab2f5dfcf391185a7aabad30ab54be9121011
Diego Lopes <diegoplopes5@gmail.com>
2022-08-03 16:40:34 -0300

    feat(DataStructures): merge two linked lists

---
+++ src/DataStructures/LinkedLists/MergeTwoLinkedLists/Solution.java
@@ -0,0 +1,134 @@
+package DataStructures.LinkedLists.MergeTwoLinkedLists;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) {
+        while (node != null) {
+            System.out.println(node.data);
+            node = node.next;
+
+            if (node != null) {
+                System.out.println(sep);
+            }
+        }
+    }
+
+    // Complete the mergeLists function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        SinglyLinkedListNode curr;
+        if (head1.data > head2.data) {
+            curr = new SinglyLinkedListNode(head2.data);
+            head2 = head2.next;
+        } else {
+            curr = new SinglyLinkedListNode(head1.data);
+            head1 = head1.next;
+        }
+        SinglyLinkedListNode newHead = curr;
+        while (head1 != null && head2 != null) {
+            if (head1.data > head2.data) {
+                curr.next = new SinglyLinkedListNode(head2.data);
+                head2 = head2.next;
+            } else {
+                curr.next = new SinglyLinkedListNode(head1.data);
+                head1 = head1.next;
+            }
+            curr = curr.next;
+        }
+        if (head1 == null) {
+            curr.next = head2;
+        } else if (head2 == null) {
+            curr.next = head1;
+        }
+        return newHead;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int tests = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int testsItr = 0; testsItr < tests; testsItr++) {
+            SinglyLinkedList llist1 = new SinglyLinkedList();
+
+            int llist1Count = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llist1Count; i++) {
+                int llist1Item = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist1.insertNode(llist1Item);
+            }
+
+            SinglyLinkedList llist2 = new SinglyLinkedList();
+
+            int llist2Count = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llist2Count; i++) {
+                int llist2Item = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist2.insertNode(llist2Item);
+            }
+
+            SinglyLinkedListNode llist3 = mergeLists(llist1.head, llist2.head);
+
+            printSinglyLinkedList(llist3, " ");
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
+
